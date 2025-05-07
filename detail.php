3de26d7101dbe1240e6b3a2b8b8e4710a702fb20
Fajrinnn-s <fjrizhr@gmail.com>
2025-05-07 12:55:25 +0700

    Create detail.php

---
+++ detail.php
@@ -0,0 +1,34 @@
+<?php
+require 'koneksi.php';
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Register</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+  </head>
+  <body>
+<h1>Detail Lengkap</h1>
+<h5>07-05-2025</h5>
+<p>Jakcson</p>
+
+
+
+
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt nulla placeat minus temporibus
+ ea totam magni modi, sequi reiciendis dignissimos. Labore recusandae iusto necessitatibus? 
+ Officiis distinctio quos enim assumenda fugiat?</p>
+ <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat eligendi nostrum corporis consequuntur, vero debitis sit excepturi dolorem ducimus officia sequi aut deserunt velit autem ullam quas, veniam facere facilis.  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis commodi ut vel! Ullam deserunt libero deleniti rem, repellat ad vero est alias accusamus sunt itaque magni saepe dicta tempora exercitationem.</p>
+
+
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+  </body>
+</html>
